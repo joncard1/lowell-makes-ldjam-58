41d68196ce5d58903c64afb1607c8e87de10c697
Jonathan Card <user.email>
2025-10-04 14:19:22 -0400

    First commit. Playing with tiling and created a character, but nothing good.

---
+++ Scripts/scene1_area.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export var speed = 1.0 # How fast the player will move (pixels/frame).
+var screen_size # Size of the game window.
+
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+	
+func _process(delta):
+	var localSpeed = speed / delta
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * localSpeed
+		$Player.transform = $Player.transform.translated(velocity)
